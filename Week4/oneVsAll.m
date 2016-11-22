a9e5c04bc4263ef438f4b4869d9cd122dea0076c
shikhaagarawal <shikhaagarawal@gmail.com>
2016-11-22 13:12:32 -0700

    Update oneVsAll.m

--- Week4/oneVsAll.m
+++ Week4/oneVsAll.m
@@ -49,17 +49,14 @@ X = [ones(m, 1) X];
 %                 initial_theta, options);
 %
 
-
-
-
-
-
-
-
-
-
-
-
+initial_theta = zeros(n + 1, 1);
+options = optimset('GradObj', 'on', 'MaxIter', 50);
+c=ones(m,1);
+for i=1:num_labels
+               
+               [theta] = fmincg (@(t)(lrCostFunction(t, X, (y == i), lambda)),initial_theta, options);
+               all_theta(i,:) = theta;
+end;
 % =========================================================================
 
 
